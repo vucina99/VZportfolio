7b38cd796c6b4644f5888d5d40c3a2b222382134
VukZdravkovic99 <vukzdravkovic69@gmail.com>
2019-09-19 23:47:20 +0200

    Start

---
+++ resources/views/index.blade.php
@@ -0,0 +1,269 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+	<meta charset="UTF-8">
+	<title>VZ-Portfolio</title>
+     
+	  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+	  <meta name="keywords" content="Vidoviti Dobrivoje, Dobrivoje, Vidovnjak, Vidovnjak Dobrivoje " />
+	  <meta name="description" content="Vidoviti dobrivoje samo za Vas, pomaze ljudima da vode normalan i zivot kakav zasluzuju bez mesanja drugih ljudi i njihovih magija " />
+	  <link rel="shortcut icon" href="img/logo.ico" />
+	  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<link href="https://fonts.googleapis.com/css?family=Playfair+Display&display=swap" rel="stylesheet">
+		<link rel="stylesheet"  href="css/style.css">
+	  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	  <meta property="og:url" content="http://vidovitidobrivoje.com/" />
+	   <meta property="og:type" content="website" />
+	   <meta property="og:title" content="Vidoviti Dobrivoje">
+	   <meta property="og:description" content="Vidoviti dobrivoje samo za Vas, pomaze ljudima da vode normalan i zivot kakav zasluzuju bez mesanja drugih ljudi i njihovih magija">
+	   <meta property="og:image" content="img/logo.jpg"/>
+  		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css">
+
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg border-bottom border-light fixed-top animated bounceInDown">
+	<div class="container">
+  <a class="navbar-brand" href="#"><img src="img/logo.jpg" alt="logo"></a>
+  <button class="navbar-toggler navbar-dark" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="#">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Ratings</a>
+      </li>
+
+    </ul>
+
+  </div>
+  </div>
+</nav>
+
+<header class="text-center">
+	<div class="radi">
+	<div class="opacity border border-light animated bounceInLeft">
+	<h1>Vuk Zdravkovic</h1><br>
+	<h2>Web Developer - Expert in Laravel <i class="fab fa-laravel"></i> / Bootstrap <i class="fab fa-bootstrap"></i></h2>
+	</div>
+	</div>
+</header>
+
+<main>
+	<a id="button"><i class="fa fa-arrow-up"></i></a>
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-6 col-md-6 text-center"><img src="img/ja.jpg" alt="Ja"></div>
+			<div class="col-lg-6 col-md-6 text-center">
+				<p>I’m a <strong>Front-end</strong> and <strong>Back-end</strong> developer from Serbia with 4 years of experience. I have a bachelor's degree in Computer Science. <br><br>
+
+				Over the last 4 years, I’ve worked on various projects - from basic HTML, custom Bootstrap themes, through to large projects in Laravel.<br><br>
+
+				My focus is to deliver a clean and reusable code and I enjoy to solve your problems.<br><br>
+
+				<strong>My skills:</strong><br>
+				- Laravel framework<br>
+				- PHP<br>
+				- Mysql<br>
+				- JavaScript(Jquery)<br>
+				- Bootstrap framework, Materialize<br>
+				- HTML<br>
+				- CSS(Sass,Less)<br><br>
+
+				I'm responsible and my goal will always be to meet your needs and deadline. I Fast learner and I always ready to learn something new from projects.<br>
+
+				<strong>Looking forward to working with you!</strong></p>
+			</div>
+		</div>
+	</div>
+	<br><br><br><br>
+	<div class="p">
+	<div class="container">
+		<div class="row ">
+			<div class="col-lg-4 col-md-6 col-sm-6 pb-4 text-center">
+				<div class="hovereffect">	
+					<img src="img/slika.png" alt="" class="img-fluid" >
+
+				        <div class="overlay">
+		           <h2 class="font-weight-bold">Blog website</h2>
+		           <a class="info" href="#" data-toggle="modal" data-target="#exampleModal2">Watch project</a>
+		        </div>
+				</div>
+			</div>
+			<div class="col-lg-4 col-md-6 col-sm-6 pb-4 text-center">
+				<div class="hovereffect">	
+					<img src="img/slika.png" alt="" class="img-fluid" >
+
+				        <div class="overlay">
+		           <h2 class="font-weight-bold">Blog website</h2>
+		           <a class="info" href="#" data-toggle="modal" data-target="#exampleModal2">Watch project</a>
+		        </div>
+				</div>
+			</div>
+			<div class="col-lg-4 col-md-6 col-sm-6 pb-4 text-center">
+				<div class="hovereffect">	
+					<img src="img/slika.png" alt="" class="img-fluid" >
+
+				        <div class="overlay">
+		           <h2 class="font-weight-bold">Blog website</h2>
+		           <a class="info" href="#" data-toggle="modal" data-target="#exampleModal2">Watch project</a>
+		        </div>
+				</div>
+			</div>
+			<div class="col-lg-4 col-md-6 col-sm-6 pb-4 text-center">
+				<div class="hovereffect">	
+					<img src="img/slika.png" alt="" class="img-fluid" >
+
+				        <div class="overlay">
+		           <h2 class="font-weight-bold">Blog website</h2>
+		           <a class="info" href="#" data-toggle="modal" data-target="#exampleModal2">Watch project</a>
+		        </div>
+				</div>
+			</div>
+			<div class="col-lg-4 col-md-6 col-sm-6 pb-4 text-center">
+				<div class="hovereffect">	
+					<img src="img/slika.png" alt="" class="img-fluid" >
+
+				        <div class="overlay">
+		           <h2 class="font-weight-bold">Blog website</h2>
+		           <a class="info" href="#" data-toggle="modal" data-target="#exampleModal2">Watch project</a>
+		        </div>
+				</div>
+			</div>
+			<div class="col-lg-4 col-md-6 col-sm-6 pb-4 text-center">
+				<div class="hovereffect">	
+					<img src="img/slika.png" alt="" class="img-fluid" >
+
+				        <div class="overlay">
+		           <h2 class="font-weight-bold">Blog website</h2>
+		           <a class="info" href="#" data-toggle="modal" data-target="#exampleModal2">Watch project</a>
+		        </div>
+				</div>
+			</div>
+
+	</div>
+	</div>
+	</div>
+	<!-- Modal -->
+	<div class="modal fade  bd-example-modal-lg" id="exampleModal2" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+	  <div class="modal-dialog modal-lg" role="document">
+	    <div class="modal-content">
+	      <div class="modal-header ">
+	        <h5 class="modal-title ml-auto" id="exampleModalLabel">Blog website</h5>
+	        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+	          <span aria-hidden="true">&times;</span>
+	        </button>
+	      </div>
+	      <div class="modal-body text-center">
+	        Neki tekst o projektu koji ce se nalaziti sigurno na sredini svega
+	      </div>
+	      <div class="modal-footer">
+	        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+	      </div>
+	    </div>
+	  </div>
+	</div>
+
+	<div class="novosti1">
+		<div class="container">
+			<div class="row"> 
+				<div class="col-lg-2 col-md-1"></div>
+				<div class="col-lg-8 col-md-10 novo ">
+					<h3 class="text-center pb-2">Keep up with new projects <i class="fas fa-paper-plane"></i></h3><br>
+					
+					<form action="" method="">
+						
+					  <div class="form-group d-flex">
+					    <label for="email"></label>
+					    <input type="email" class="form-control prvi" id="email" placeholder="Your email" >
+
+					     <input type="submit" class="btn btn-outline-dark drugi" value="Send" >
+					  </div>
+
+					  
+					</form>
+
+					</div>
+				
+				<div class="col-lg-2 col-md-1"></div>
+			</div>
+		</div>
+	</div>
+
+		<div class="slika">
+			<div class="pozadina">
+				<div class="container ">
+					<div class="row">
+
+						<div class="col-lg-2 col-md-2 col-sm-12"></div>
+						<div class="col-lg-8 col-md-8 col-sm-12 kontis border border-light ">
+						<br>
+							<h4 class="text-center">Contant me <i class="fa fa-envelope"></i></h4>
+						
+						<form action="" method="" class="pr-3 pl-3">
+						
+						  <div class="form-group  ">
+						    <label for="name"></label>
+						    <input type="text" class="form-control" id="name" placeholder="Your name" >
+						  </div>
+						  <div class="form-group  ">
+						    <label for="email"></label>
+						    <input type="email" class="form-control" id="email" placeholder="Your email" >
+						  </div>
+						  <div class="form-group  ">
+						    <label for="question"></label>
+						    <textarea name="" id="" cols="30" rows="10" id="question" class="form-control" placeholder="Your question"></textarea>
+						  </div>
+						  <input type="submit" class="btn btn-outline-light btn-lg " value="Send" >
+						   <input type="reset" class="btn btn-outline-light btn-lg " value="Reset" >
+					</form>
+						<br><br>
+						</div>
+						<div class="col-lg-2 col-md-2 col-sm-12"></div>
+					</div>
+				</div>
+			</div>
+		</div>
+</main>
+<footer>
+	<div class="container">
+		<div class="row text-light text-center ae"> 
+			<div class="col-lg-3 col-md-6 "><br><br><br><h6>Social networks <i class="fa fa-globe"></i> </h6>
+				<div class="mreze">
+				<a href=""><i class="fab fa-facebook-square"></i></a>
+					<a href=""><i class="fab fa-instagram"></i></a>		
+						<a href=""><i class="fab fa-linkedin"></i></a>	
+				</div>
+			</div>
+
+			<div class="col-lg-3 col-md-6"><br><br><br><h6>Email <i class="fa fa-envelope"></i> </h6>
+				<p>vukzdravkovic69@gmail.com</p>
+			</div>
+			<div class="col-lg-3 col-md-6"><br><br><br><h6>Mobile <i class="fa fa-mobile"></i> </h6>
+				<p>+381628377040</p>
+			</div>
+			<div class="col-lg-3 col-md-6"><br><br><br><h6>interesting <i class="fas fa-smile"></i> </h6>
+				<p>You are CSS3 in my HTML5</p>
+			</div>
+		</div>
+	</div>
+	<div class="container-fluid aj">
+		<div class="row">
+			<div class="col-lg-12 text-light text-center"><br>
+				<p>Copyright &copy; 2019 Portfolio Vuk Zdravkovic. All Rights Reserved. Credits: Vuk Zdravković &nbsp;  <a href="sitemap.xml" class="text-light"><i class="fas fa-sitemap"></i></a></p>
+			</div>
+		</div>
+	</div>
+</footer>
+
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>	
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="js/index.js"></script>
+
+</body>
+</html>
